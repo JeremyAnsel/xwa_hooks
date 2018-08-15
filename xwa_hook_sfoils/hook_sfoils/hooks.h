73b68086f588a65680768a22f44dabbed60013ee
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2018-08-15 14:30:36 +0200

    hook_sfoils: Align camera with the bridge

--- xwa_hook_sfoils/hook_sfoils/hooks.h
+++ xwa_hook_sfoils/hook_sfoils/hooks.h
@@ -20,8 +20,16 @@ static const HookPatchItem g_defineSFoilsPatch[] =
 	{ 0x000D14, "4883F8310F87B101000033D28A90C42C4000", "50E816721A0083C4048BF8E94E0100009090" },
 };
 
+static const HookPatchItem g_alignCameraBridgePatch[] =
+{
+	{ 0x08058C, "0F85F5000000", "909090909090" },
+	{ 0x080D7C, "0F85E0000000", "909090909090" },
+	{ 0x08304B, "0F85E9000000", "909090909090" },
+};
+
 static const HookPatch g_patches[] =
 {
 	MAKE_HOOK_PATCH("Disable S-Foil model filter", g_disableSFoilModelFilterPatch),
 	MAKE_HOOK_PATCH("To call the hook that defines S-Foils", g_defineSFoilsPatch),
+	MAKE_HOOK_PATCH("Align the camera with the bridge", g_alignCameraBridgePatch),
 };
