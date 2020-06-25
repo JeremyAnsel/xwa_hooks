b2bbbb2ee2561fae8b9dea7eb1c469b262f5f5f9
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2020-06-25 20:59:25 +0200

    hook_joystick_ff: allow small movements

--- xwa_hook_joystick_ff/hook_joystick_ff/hooks.h
+++ xwa_hook_joystick_ff/hook_joystick_ff/hooks.h
@@ -11,10 +11,19 @@ static const HookFunction g_hookFunctions[] =
 static const HookPatchItem g_joystickEnumPatch[] =
 {
 	{ 0x0349D0, "83EC108B44241456", "E85B351700C20800" },
+};
+
+static const HookPatchItem g_joystickSmallMovementsPatch[] =
+{
 	{ 0x0338A0, "51", "C3" },
+	{ 0x10AE69, "7E22", "9090" },
+	{ 0x10AEAE, "7E2F", "9090" },
+	{ 0x10AF00, "7E28", "9090" },
+	{ 0x10AF4B, "7E3E", "9090" },
 };
 
 static const HookPatch g_patches[] =
 {
 	MAKE_HOOK_PATCH("To call the hook that selects the joystick ff device", g_joystickEnumPatch),
+	MAKE_HOOK_PATCH("To call the hook that allows joystick small movements", g_joystickSmallMovementsPatch),
 };
--- xwa_hook_joystick_ff/zip/readme.txt
+++ xwa_hook_joystick_ff/zip/readme.txt
@@ -21,7 +21,13 @@ The following modifications are applied at runtime to xwingalliance.exe:
 
 # To call the hook that selects the joystick ff device
 At offset 0349D0, replace 83EC108B44241456 with E85B351700C20800.
+
+# To call the hook that allows joystick small movements
 At offset 0338A0, replace 51 with C3.
+At offset 10AE69, replace 7E22 with 9090.
+At offset 10AEAE, replace 7E2F with 9090.
+At offset 10AF00, replace 7E28 with 9090.
+At offset 10AF4B, replace 7E3E with 9090.
 
 
 *** Usage ***
