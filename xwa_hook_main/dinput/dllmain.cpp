8a0de858ef8e708f7034969aea55c0aa52e73fa1
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2022-04-03 15:24:30 +0200

    hook_main: create dump on crash

--- xwa_hook_main/dinput/dllmain.cpp
+++ xwa_hook_main/dinput/dllmain.cpp
@@ -2,6 +2,90 @@
 #include "hook.h"
 #include "hook_function.h"
 #include "hooks.h"
+#include <string>
+#include <Windows.h>
+#include <DbgHelp.h>
+
+#pragma comment(lib, "Dbghelp")
+
+std::string int_to_hex(int i);
+
+std::string GetModuleName(void* address)
+{
+	HMODULE hModule = NULL;
+	GetModuleHandleEx(
+		GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+		(LPCTSTR)address,
+		&hModule);
+
+	char moduleFileName[MAX_PATH];
+	GetModuleFileName(hModule, moduleFileName, sizeof(moduleFileName));
+	std::string moduleName = std::string(strrchr(moduleFileName, '\\') + 1);
+
+	return moduleName;
+}
+
+void MakeMinidump(EXCEPTION_POINTERS* pExceptionInfo)
+{
+	char name[MAX_PATH];
+	GetModuleFileName(nullptr, name, sizeof(name));
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+
+	std::string moduleName = GetModuleName(pExceptionInfo->ExceptionRecord->ExceptionAddress);
+	moduleName = moduleName.substr(0, moduleName.rfind('.'));
+
+	wsprintf(
+		name + strlen(name) - 4,
+		"_%4d%02d%02d_%02d%02d%02d_%s.dmp",
+		time.wYear,
+		time.wMonth,
+		time.wDay,
+		time.wHour,
+		time.wMinute,
+		time.wSecond,
+		moduleName.c_str());
+
+	HANDLE hFile = CreateFile(name, GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		return;
+	}
+
+	MINIDUMP_EXCEPTION_INFORMATION exceptionInfo{};
+	exceptionInfo.ThreadId = GetCurrentThreadId();
+	exceptionInfo.ExceptionPointers = pExceptionInfo;
+	exceptionInfo.ClientPointers = FALSE;
+
+	MiniDumpWriteDump(
+		GetCurrentProcess(),
+		GetCurrentProcessId(),
+		hFile,
+		MINIDUMP_TYPE(MiniDumpWithCodeSegs | MiniDumpWithDataSegs),
+		pExceptionInfo ? &exceptionInfo : nullptr,
+		nullptr,
+		nullptr);
+
+	CloseHandle(hFile);
+
+	OutputDebugString((std::string() + "A crash dump has been generated at " + name).c_str());
+}
+
+LONG WINAPI TopLevelExceptionHandler(PEXCEPTION_POINTERS pExceptionInfo)
+{
+	std::string text;
+	text.append("Exception 0x");
+	text.append(int_to_hex(pExceptionInfo->ExceptionRecord->ExceptionCode));
+	text.append(" at ");
+	text.append(int_to_hex((int)pExceptionInfo->ExceptionRecord->ExceptionAddress));
+	text.append("  in ");
+	text.append(GetModuleName(pExceptionInfo->ExceptionRecord->ExceptionAddress));
+
+	OutputDebugString(text.c_str());
+	MakeMinidump(pExceptionInfo);
+
+	return EXCEPTION_CONTINUE_SEARCH;
+}
 
 bool IsXwaExe()
 {
@@ -58,6 +142,8 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
 			}
 
 			VirtualProtectMemoryExecuteReadWrite();
+
+			SetUnhandledExceptionFilter(TopLevelExceptionHandler);
 		}
 
 		break;
