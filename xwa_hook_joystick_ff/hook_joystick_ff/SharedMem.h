0fb5f53658792cc69938289f8522c8b3a8aee1b3
Leo Reyes <Leo.H.Reyes@gmail.com>
2022-12-29 17:10:35 -0800

    Add code needed to make this hook compatible with the gimbal lock fix.
    JoystickRoll was added to SharedMem. This is now used by the gimbal lock fix
    code. Also added the following:
    
    // If the gimbal lock fix is active, the following will disable the joystick
    // (default: false). Set by ddraw
    bool GimbalLockFixActive;
    // Set by ddraw when joystick emulation is enabled
    bool JoystickEmulationEnabled;
    // Set by the joystick hook
    bool JoystickHookPresent;

--- xwa_hook_joystick_ff/hook_joystick_ff/SharedMem.h
+++ xwa_hook_joystick_ff/hook_joystick_ff/SharedMem.h
@@ -6,11 +6,22 @@
 struct SharedMemDataJoystick {
 	// Joystick's position, written by the joystick hook, or by the joystick emulation code.
 	// These values are normalized in the range -1..1
-	float JoystickYaw, JoystickPitch;
+	float JoystickYaw, JoystickPitch, JoystickRoll;
+	// If the gimbal lock fix is active, the following will disable the joystick (default: false)
+	// Set by ddraw
+	bool GimbalLockFixActive;
+	// Set by ddraw when joystick emulation is enabled
+	bool JoystickEmulationEnabled;
+	// Set by the joystick hook
+	bool JoystickHookPresent;
 
 	SharedMemDataJoystick() {
 		JoystickYaw = 0.0f;
 		JoystickPitch = 0.0f;
+		JoystickRoll = 0.0f;
+		GimbalLockFixActive = false;
+		JoystickEmulationEnabled = false;
+		JoystickHookPresent = true;
 	}
 };
 
