bc4696b91777db7f644df7a7a1829c709232e2b3
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2023-01-21 21:54:11 +0100

    Revert "hook_weapon_rate: support gun turrets"
    This reverts commit d070e97d24be6757df815b09a9f7da7c8ad3425f.

--- xwa_hook_weapon_rate/hook_weapon_rate/hooks.h
+++ xwa_hook_weapon_rate/hook_weapon_rate/hooks.h
@@ -8,9 +8,6 @@ static const HookFunction g_hookFunctions[] =
 	{ 0x491656, WeaponDechargeHook },
 	{ 0x4908D4, WeaponRechargeHook },
 	{ 0x49186B, WeaponCooldownTimeHook },
-	{ 0x4915F6, WeaponRackHook },
-	{ 0x491C03, WeaponRackHook },
-	{ 0x4E1C49, WeaponHardpointHook },
 };
 
 static const HookPatchItem g_weaponRatePatch[] =
@@ -20,15 +17,7 @@ static const HookPatchItem g_weaponRatePatch[] =
 	{ 0x090C66, "A1FC0D910083FBFF", "E8B5721100EB2990" },
 };
 
-static const HookPatchItem g_weaponRacksPatch[] =
-{
-	{ 0x0909F1, "E8BA080000", "E83A751100" },
-	{ 0x090FFE, "E8AD020000", "E82D6F1100" },
-	{ 0x0E1044, "E88763FAFF", "E8E76E0C00" },
-};
-
 static const HookPatch g_patches[] =
 {
 	MAKE_HOOK_PATCH("To call the hook that defines weapon decharge and recharge rates", g_weaponRatePatch),
-	MAKE_HOOK_PATCH("To call the hook that filters weapon racks", g_weaponRacksPatch),
 };
--- xwa_hook_weapon_rate/hook_weapon_rate/weapon.h
+++ xwa_hook_weapon_rate/hook_weapon_rate/weapon.h
@@ -3,6 +3,3 @@
 int WeaponDechargeHook(int* params);
 int WeaponRechargeHook(int* params);
 int WeaponCooldownTimeHook(int* params);
-
-int WeaponRackHook(int* params);
-int WeaponHardpointHook(int* params);
